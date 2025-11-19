7879894ce612572a21e5f33004d8e97c49e80312
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-11-19 14:35:18 -0300

    program try catch

--- Locadora.View/Program.cs
+++ Locadora.View/Program.cs
@@ -3,7 +3,7 @@ using Locadora.Models;
 using Microsoft.Data.SqlClient;
 using Utils.Databases;
 
-Cliente cliente = new Cliente("Novo Clientezn", "1novoemail@uol.com");
+Cliente cliente = new Cliente("Novo Clientezin", "1novo0email@uol.com");
 
 //Documento documento = new Documento(1, "RG", "123456789", new DateOnly(2015, 5, 1), new DateOnly(2025, 5, 1));
 
@@ -11,8 +11,14 @@ Console.WriteLine(cliente);
 
 var clienteController = new ClienteController();
 
-clienteController.AdicionarCliente(cliente);
-
+try
+{
+    clienteController.AdicionarCliente(cliente);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 var listaDeClientes = clienteController.ListarTodosClientes();
 
 foreach (var clienteDaLista in listaDeClientes)
