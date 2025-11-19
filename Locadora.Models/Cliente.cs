67a51db7fb91c86aab4c82a6f67b65a2bcd0a19b
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-11-19 10:07:34 -0300

    Add project files.

---
+++ Locadora.Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿namespace Locadora.Models
+{
+    public class Cliente
+    {
+
+        public int ClienteID { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string? Telefone { get; private set; } = String.Empty;
+        public Cliente(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public Cliente(string nome, string email, string? telefone) : this(nome, email)
+        {
+            Telefone = telefone;
+        }
+    }
+}
