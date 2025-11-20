54180e0023a624322c7086bce47f166599f9c29e
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-11-20 15:55:29 -0300

    veiculo e categoria

---
+++ Locadora.Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Categoria
+    {
+
+        public int CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public decimal Diaria { get; private set; }
+        public Categoria(string nome, string descricao, decimal diaria)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Diaria = diaria;
+        }
+    }
+}
